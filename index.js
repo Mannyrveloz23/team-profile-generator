b63bb90cf349e9047fb937cc334187087b0aca22
mannyrveloz23 <you@example.com>
2023-02-02 23:27:16 -0500

    html generator, prumpt creation

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const htmlgenerator = require('./src/htmlgenerator');
+const path = require('path');
+const { type } = require('os');
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
+const outputPath = path.join(OUTPUT_DIR, "index.html");
+const team = []
+
+const promptManager = () => {
+return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Write your name",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Write your id",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Write your email",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Write your office numer",
+        }
+    
+]).then(answers => {
+    const manager = new Manager(
+        answers.name, answers.input, answers.email, answers.officeNumber
+    )
+    team.push(manager);
+    addEmployee();
+})
+}
+
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'roleChoices',
+            message: 'Choose a profile option',
+            choices: ['Add engineer', 'Add intern', 'Team complete']
+        }
+    ]).then(userChoice => {
+        switch (userChoice.roleChoices) {
+            case 'Add engineer':
+            promptEngineer();
+            break;
+            case 'Add intern':
+            promptIntern();
+            break;
+            default:
+            builtTeam();
+
+        }
+    })
+    
+
+}
